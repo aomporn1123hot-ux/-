6ecfa365192a5be64c9b22ab909b6cc365783569
aomporn1123hot-ux <aomporn1123.hot@gmail.com>
2025-11-03 04:05:37 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,9 @@
+// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
 import { getDatabase, ref, set, push } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-database.js";
 
-const firebaseConfig = {
+// ----- โปรเจกต์เก่า -----
+const oldConfig = {
   apiKey: "AIzaSyDf0D2GLLDHoAVX4zq-tLuVocSmsrFhs38",
   authDomain: "fera-2215e.firebaseapp.com",
   databaseURL: "https://fera-2215e-default-rtdb.asia-southeast1.firebasedatabase.app",
@@ -10,8 +12,21 @@ const firebaseConfig = {
   messagingSenderId: "810225127285",
   appId: "1:810225127285:web:fa87166d4e3e4770670d3c"
 };
+const oldApp = initializeApp(oldConfig, "oldApp");
+const oldDB = getDatabase(oldApp);
 
-const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
+// ----- โปรเจกต์ใหม่ -----
+const newConfig = {
+  apiKey: "AIzaSyBbWzOxGQa7OKb6hXWvx2o4ZcimTqWqtg4",
+  authDomain: "aumporn-03.firebaseapp.com",
+  databaseURL: "https://aumporn-03-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "aumporn-03",
+  storageBucket: "aumporn-03.firebasestorage.app",
+  messagingSenderId: "960384618927",
+  appId: "1:960384618927:web:cd333e03026bb56f2f1f5a",
+  measurementId: "G-YYKZVQS3NX"
+};
+const newApp = initializeApp(newConfig, "newApp");
+const newDB = getDatabase(newApp);
 
-export { db, ref, set, push };
+export { oldDB, newDB, ref, set, push };
